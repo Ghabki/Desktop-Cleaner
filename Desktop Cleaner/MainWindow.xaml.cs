e5b69f2abf997ea1b348d67504dfa31e1894fdaf
Gabriel <utenkar@gmail.com>
2020-01-08 18:03:25 +0100

    Only save
    Just saving the files

--- Desktop Cleaner/MainWindow.xaml.cs
+++ Desktop Cleaner/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 
 using System.Data.SQLite;
 using Microsoft.Win32;
+using System.Windows.Forms;
 
 namespace Desktop_Cleaner {
     public partial class MainWindow : Window {
@@ -26,7 +27,6 @@ namespace Desktop_Cleaner {
         }
         #endregion
 
-
         #region Button_clicks
         private void Clean_Button_Click(object sender, RoutedEventArgs e) {
             //gre po podatkih in premakne kar mora in kaj ne
@@ -35,46 +35,48 @@ namespace Desktop_Cleaner {
         private void Remove_Button_Click(object sender, RoutedEventArgs e) {
             //zbrise označeni file name
 
-
-
             Remove_file();
         }
 
-        public void Add_Button_Button_Click(object sender, RoutedEventArgs e) {
+        private void Add_Button_Button_Click(object sender, RoutedEventArgs e) {
             String File_name = String.Empty;
+
+            //todo add gumb za file in folder dialog rak
             try{
-                OpenFileDialog dlg = new OpenFileDialog {
+                System.Windows.Forms.OpenFileDialog dlg = new System.Windows.Forms.OpenFileDialog {
                     InitialDirectory = "Desktop"
-                };
+            };
 
-                if (dlg.ShowDialog() == true) {
+                if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
                     File_name = dlg.FileName;
-                    MessageBox.Show(File_name);
+                    System.Windows.MessageBox.Show(File_name);
                     Add_file(File_name);
-                    
-
                 }
             }
             catch (Exception ex){
-                MessageBox.Show("FileDialog Error: " + ex, "FileDialog Error", MessageBoxButton.OK, MessageBoxImage.Error);
-
+                System.Windows.MessageBox.Show("FileDialog Error: " + ex, "FileDialog Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         }
 
+        private void Add_button_folder(object sender, RoutedEventArgs e) {
+            String File_name = String.Empty;
+
+                //todo add gumb za file in folder dialog rak
+                try {
+                FolderBrowserDialog lol = new FolderBrowserDialog {
+                    SelectedPath = "Desktop"
+                };
+
+                if (lol.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                    System.Windows.MessageBox.Show("You selected: " + lol.SelectedPath);
+                    File_name = lol.SelectedPath;
+                    Add_file(File_name);
+                }
+            }
+            catch (Exception ex){
+                System.Windows.MessageBox.Show("FolderDialog Error: " + ex, "FileDialog Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
 
 
         #endregion
@@ -90,9 +92,64 @@ namespace Desktop_Cleaner {
 
 
         #region Init
+
+        private void Init() {
+            Database_innit();
+            Listbox_innit();
+            List_Files.Items.Add("1");
+            List_Files.Items.Add("2");
+            List_Files.Items.Add("3");
+            List_Files.Items.Add("4");
+
+        }
+        //TODO naredi da z db pobere imena filow in da v list box
+
+
+
+        #endregion
+
+        #region Functions
+        private void Add_file(String file_name) {
+            string prilepi = string.Empty;
+
+            using (SQLiteCommand cmd = new SQLiteCommand(sqlite_conn)) {
+
+                cmd.CommandText = @"INSERT INTO File_names (name) VALUES (@datoteka);";
+                cmd.Parameters.AddWithValue("@datoteka", file_name);
+                cmd.Prepare();
+                cmd.ExecuteNonQuery();
+
+                //List_Files.Items.Add("asg");
+                prilepi = Refactor_string(file_name);
+                List_Files.Items.Add(prilepi);
+
+            }
+
+        }
+
+        private void Remove_file() {
+            String item = string.Empty;
+            try {
+                string item = List_Files.SelectedItem.ToString();
+
+                List_Files.Items.RemoveAt(List_Files.SelectedIndex);
+            }
+            catch (Exception ex) {
+                System.Windows.MessageBox.Show("You need to select a line to delete it.", "Reminder", MessageBoxButton.OK, MessageBoxImage.Information);
+                Console.WriteLine(ex);
+            }
+
+
+            //System.Windows.MessageBox.Show(item, "FileDialog Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+
+
+        }
+
+
         // more biti vzunaj da druga metoda to najde
         SQLiteConnection sqlite_conn = null;
-        public void Init() { 
+        private void Database_innit() {
             sqlite_conn = new SQLiteConnection("Data Source=../../Data/Data.db; Version = 3");
             try {
                 sqlite_conn.Open();
@@ -101,44 +158,27 @@ namespace Desktop_Cleaner {
             catch (Exception ex) {
                 System.Console.WriteLine(ex);
                 sqlite_conn.Close();
-                MessageBox.Show("Database error: "+ex, "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                System.Windows.MessageBox.Show("Database error: " + ex, "Database Error", MessageBoxButton.OK, MessageBoxImage.Error);
                 System.Environment.Exit(1);
             }
-            //todo remove ta close
-
-            
-            }
-            //TODO naredi da z db pobere imena filow in da v list box
-
-        
-
-        #endregion
-
-        #region Functions
-        public void Add_file(String file_name) {
-            using (SQLiteCommand cmd = new SQLiteCommand(sqlite_conn)) {
+        }
 
 
-                cmd.CommandText = @"INSERT INTO File_names (name) VALUES (@datoteka);";
-                cmd.Parameters.AddWithValue("@datoteka", file_name);
-                cmd.Prepare();
-                cmd.ExecuteNonQuery();
-
-                //List_Files.Items.Add("asg");
-                List_Files.Items.Add("fdsfsdf");
-
-            }
+        private void Listbox_innit() {
 
         }
-        private void Remove_file() {
-
+        private String Refactor_string(String name) {
+            string[] splitan = name.Split('\\');
+            return splitan[4];
         }
+
+
+
 
 
 
         #endregion
 
-    
 
     }
 }
